c7b24a58407a11766f1cbce8ee3a0f585ba828db
André Santos <andressouza25@gmail.com>
2025-05-29 19:34:37 -0300

    refactor: move user use case to its own folder

---
+++ src/use-cases/user/update-user.js
@@ -0,0 +1,33 @@
+import { EmailAlreadyInUseError } from '../../errors/user.js'
+import bcrypt from 'bcrypt'
+
+export class UpdateUserUseCase {
+    constructor(getUserByEmailRepository, updateUserRepository) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+        this.updateUserRepository = updateUserRepository
+    }
+    async execute(userId, updateUserParams) {
+        const userWithProvidedEmail =
+            await this.getUserByEmailRepository.execute(updateUserParams.email)
+
+        if (userWithProvidedEmail && userWithProvidedEmail.id !== userId) {
+            throw new EmailAlreadyInUseError(updateUserParams.email)
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+            user.password = hashedPassword
+        }
+
+        const updateUser = await this.updateUserRepository.execute(userId, user)
+
+        return updateUser
+    }
+}
