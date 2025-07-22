ae1b9f106bdced34fab5b3d31175424c5e011dba
André Santos <andressouza25@gmail.com>
2025-07-22 19:03:50 -0300

    test: ensure GetUserBalanceUseCase get a balance successfully

---
+++ src/use-cases/user/get-user-balance.test.js
@@ -0,0 +1,55 @@
+import { faker } from '@faker-js/faker'
+import { GetUserBalanceUseCase } from './get-user-balance.js'
+
+describe('GetUserBalanceUseCase', () => {
+    const userBalance = {
+        earnings: faker.finance.amount(),
+        expenses: faker.finance.amount(),
+        investments: faker.finance.amount(),
+        balance: faker.finance.amount(),
+    }
+
+    class GetUserBalanceRepositoryStub {
+        async execute() {
+            return userBalance
+        }
+    }
+    class GetUserByIdRepositoryStub {
+        async execute() {
+            return {
+                id: faker.string.uuid(),
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 7,
+                }),
+            }
+        }
+    }
+    const makeSut = () => {
+        const getUserBalanceRepository = new GetUserBalanceRepositoryStub()
+        const getUserByIdRepository = new GetUserByIdRepositoryStub()
+        const sut = new GetUserBalanceUseCase(
+            getUserBalanceRepository,
+            getUserByIdRepository,
+        )
+
+        return {
+            getUserByIdRepository,
+            getUserBalanceRepository,
+            sut,
+        }
+    }
+
+    it('should get user balance successfully', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute(faker.string.uuid())
+
+        // Assert
+        expect(result).toEqual(userBalance)
+    })
+})
