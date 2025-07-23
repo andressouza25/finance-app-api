1b1c6dea3fc3e6b4a6a3126d49bee1ab3f915643
André Santos <andressouza25@gmail.com>
2025-07-23 19:39:19 -0300

    refactor: ensure transaction controllers use transaction fixtures

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -1,17 +1,11 @@
 import { faker } from '@faker-js/faker'
 import { DeleteTransactionController } from './delete-transaction.js'
+import { transaction } from '../../tests'
 
 describe('DeleteTransactionController', () => {
     class DeleteTransactionUseCaseStub {
         async execute() {
-            return {
-                user_id: faker.string.uuid(),
-                id: faker.string.uuid(),
-                name: faker.commerce.productName(),
-                date: faker.date.anytime().toISOString(),
-                type: 'EXPENSE',
-                amount: Number(faker.finance.amount()),
-            }
+            return transaction
         }
     }
     const makeSut = () => {
