408bc89183dfa7ba52bf28f901a7962e7dd78237
André Santos <andressouza25@gmail.com>
2025-07-30 19:31:48 -0300

    test: ensure PostgresGetTransactionByUserIdRepository throw if Prisma throws

--- src/repositories/postgres/transaction/get-transaction-by-user.test.js
+++ src/repositories/postgres/transaction/get-transaction-by-user.test.js
@@ -42,4 +42,14 @@ describe('PostgresGetTransactionByUserIdRepository', () => {
             },
         })
     })
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresGetTransactionsByUserIdRepository()
+        jest.spyOn(prisma.transaction, 'findMany').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const promise = sut.execute(user.id)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
