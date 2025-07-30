3f39e3a8094dbe6020310f7ce5b10bc01866c1ae
André Santos <andressouza25@gmail.com>
2025-07-30 18:59:59 -0300

    test: ensure PostgresCreateTransactionRepository create a transaction on db

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,11 @@ const config = {
     coverageDirectory: 'coverage',
     coverageProvider: 'v8',
     collectCoverageFrom: ['src/**/*.js', '!**/generated/**'],
-    watchPathIgnorePatterns: ['<rootDir>/postgres-data/'],
+    watchPathIgnorePatterns: [
+        '<rootDir>/postgres-data/',
+        '<rootDir>/node_modules/',
+        '<rootDir>/coverage/',
+    ],
     globalSetup: '<rootDir>/jest.global-setup.js',
     setupFilesAfterEnv: ['<rootDir>/jest.setup-after-env.js'],
 }
