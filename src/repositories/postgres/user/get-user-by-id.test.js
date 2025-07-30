079e5c3f332a99230d89a4fd7673ff7c6def771d
André Santos <andressouza25@gmail.com>
2025-07-30 17:25:23 -0300

    test: ensure PostgresGetUserByIdRepository gets user by id on db

---
+++ src/repositories/postgres/user/get-user-by-id.test.js
@@ -0,0 +1,15 @@
+import { prisma } from '../../../../prisma/prisma'
+import { user as fakeUser } from '../../../tests'
+import { PostgresGetUserByIdRepository } from './get-user-by-id'
+
+describe('PostgersGetUserByIdRepository', () => {
+    it('should get user by id on db', async () => {
+        const user = await prisma.user.create({ data: fakeUser })
+
+        const sut = new PostgresGetUserByIdRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result).toStrictEqual(user)
+    })
+})
