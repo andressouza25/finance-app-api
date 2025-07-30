b09d66cc67ace8ee47ec6d5df8cc0ae8f44427b0
André Santos <andressouza25@gmail.com>
2025-07-30 17:41:28 -0300

    test: ensure PostgresGetUserBalanceRepository throw with Prisma throws

--- src/repositories/postgres/user/get-user-balance.test.js
+++ src/repositories/postgres/user/get-user-balance.test.js
@@ -98,4 +98,14 @@ describe('PostgresGetUserBalanceRepository', () => {
             },
         })
     })
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresGetUserBalanceRepository()
+        jest.spyOn(prisma.transaction, 'aggregate').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const promise = sut.execute(fakeUser.id)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
