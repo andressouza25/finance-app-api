987225f892e82d39035ea9ca86b8640d4b21daa3
André Santos <andressouza25@gmail.com>
2025-08-05 19:34:50 -0300

    test: ensure make's returns valids controllers

---
+++ src/factories/controllers/user.test.js
@@ -0,0 +1,36 @@
+import {
+    CreateUserController,
+    GetUserByIdController,
+    UpdateUserController,
+    DeleteUserController,
+    GetUserBalanceController,
+} from '../../controllers'
+import {
+    makeCreateUserController,
+    makeGetUserByIdController,
+    makeUpdateUserController,
+    makeDeleteUserController,
+    makeGetUserBalanceController,
+} from './user'
+
+describe('UserControllerFactories', () => {
+    it('should  return a valid GetUserByIdController instance', () => {
+        expect(makeGetUserByIdController()).toBeInstanceOf(
+            GetUserByIdController,
+        )
+    })
+    it('should  return a valid CreateUserController instance', () => {
+        expect(makeCreateUserController()).toBeInstanceOf(CreateUserController)
+    })
+    it('should  return a valid UpdateUserController instance', () => {
+        expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController)
+    })
+    it('should  return a valid DeleteUserController instance', () => {
+        expect(makeDeleteUserController()).toBeInstanceOf(DeleteUserController)
+    })
+    it('should  return a valid GetUserBalanceController instance', () => {
+        expect(makeGetUserBalanceController()).toBeInstanceOf(
+            GetUserBalanceController,
+        )
+    })
+})
