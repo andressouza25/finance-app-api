5be8a5c45737b8d054548f3a4f3ede67239d2a79
André Santos <andressouza25@gmail.com>
2025-08-05 19:18:07 -0300

    feat: ensure DeleteUserController return 404 if UserNotFoundError is throw

--- src/controllers/user/delete-user.js
+++ src/controllers/user/delete-user.js
@@ -5,7 +5,7 @@ import {
     ok,
     userNotFoundResponse,
 } from '../helpers/index.js'
-
+import { UserNotFoundError } from '../../errors/index.js'
 export class DeleteUserController {
     constructor(deleteUserUseCase) {
         this.deleteUserUseCase = deleteUserUseCase
@@ -20,12 +20,11 @@ export class DeleteUserController {
 
             const deletedUser = await this.deleteUserUseCase.execute(userId)
 
-            if (!deletedUser) {
-                return userNotFoundResponse()
-            }
-
             return ok(deletedUser)
         } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
             console.log(error)
             return serverError()
         }
