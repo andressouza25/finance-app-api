e9ce6a9b3394e653f681867e79c81cafef01e637
André Santos <andressouza25@gmail.com>
2025-08-11 16:00:46 -0300

    test: ensure DELETE /api/transaction?userId  returns 200 when deleting a transaction

--- src/routes/transactions.e2e.test.js
+++ src/routes/transactions.e2e.test.js
@@ -40,7 +40,7 @@ describe('Transaction Routes E2E Test', () => {
         expect(response.body[0].id).toBe(createdTransaction.id)
     })
 
-    it('PATCH /api/transactionId should return 200 when updating a transaction successfully', async () => {
+    it('PATCH /api/transaction/:trasactionId should return 200 when updating a transaction successfully', async () => {
         const { body: createdUser } = await request(app)
             .post('/api/users')
             .send({
@@ -58,5 +58,24 @@ describe('Transaction Routes E2E Test', () => {
         expect(response.status).toBe(200)
         expect(response.body.amount).toBe('100')
         expect(response.body.type).toBe(TransactionType.INVESTMENT)
+    })
+
+    it('DELETE /api/transaction/:trasactionId should return 200 when deleting a transaction successfully', async () => {
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+        const { body: createdTransaction } = await request(app)
+            .post('/api/transactions')
+            .send({ ...transaction, user_id: createdUser.id, id: undefined })
+
+        const response = await request(app).delete(
+            `/api/transactions/${createdTransaction.id}`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body.id).toBe(createdTransaction.id)
     })
 })
