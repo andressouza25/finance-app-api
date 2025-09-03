78f3031de1ab6940c5defcfe3c709616b1364724
André Santos <andressouza25@gmail.com>
2025-09-03 14:55:52 -0300

    feat: add LoginUserController

---
+++ src/controllers/user/login-user.js
@@ -0,0 +1,43 @@
+import { InvalidPasswordError, UserNotFoundError } from '../../errors/user.js'
+import { loginSchema } from '../../schemas/user.js'
+import {
+    badRequest,
+    notFound,
+    ok,
+    serverError,
+    unauthorized,
+} from '../helpers/index.js'
+import { ZodError } from 'zod'
+
+export class LoginUserController {
+    constructor(loginUserUseCase) {
+        this.loginUserUseCase = loginUserUseCase
+    }
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+            await loginSchema.parseAsync(params)
+            const user = await this.loginUserUseCase.execute(
+                params.email,
+                params.pasword,
+            )
+            return ok(user)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.issues[0].message,
+                })
+            }
+
+            if (error instanceof InvalidPasswordError) {
+                return unauthorized()
+            }
+            if (error instanceof UserNotFoundError) {
+                return notFound({
+                    message: 'User not found.',
+                })
+            }
+            return serverError()
+        }
+    }
+}
