64475d94bc818cc34971f400dbb753780f60ab70
André Santos <andressouza25@gmail.com>
2025-09-05 14:36:34 -0300

    test: add RefreshTokenUseCase

---
+++ src/use-cases/user/refresh-token.test.js
@@ -0,0 +1,57 @@
+import { UnauthorizedError } from '../../errors/user'
+import { RefreshTokensUseCase } from './refresh-token'
+
+describe('RefreshTokenUseCase', () => {
+    class TokenVerifierAdapterStub {
+        execute() {
+            return true
+        }
+    }
+
+    class TokensGeneratorAdapterStub {
+        execute() {
+            return {
+                accessToken: 'any_access_token',
+                refreshToken: 'any_refresh_token',
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const tokenVerifierAdapter = new TokenVerifierAdapterStub()
+        const tokensGeneratorAdapter = new TokensGeneratorAdapterStub()
+        const sut = new RefreshTokensUseCase(
+            tokensGeneratorAdapter,
+            tokenVerifierAdapter,
+        )
+
+        return {
+            sut,
+            tokensGeneratorAdapter,
+            tokenVerifierAdapter,
+        }
+    }
+
+    it('should return new tokens', () => {
+        const { sut } = makeSut()
+        const refreshToken = 'any_refresh_token'
+        const result = sut.execute(refreshToken)
+        expect(result).toEqual({
+            accessToken: 'any_access_token',
+            refreshToken: 'any_refresh_token',
+        })
+    })
+
+    it('should throw if tokenVerifierAdapter throws', () => {
+        const { sut, tokenVerifierAdapter } = makeSut()
+        import.meta.jest
+            .spyOn(tokenVerifierAdapter, 'execute')
+            .mockImplementationOnce(() => {
+                throw new Error()
+            })
+
+        expect(() => sut.execute('any_refresh_token')).toThrow(
+            new UnauthorizedError(),
+        )
+    })
+})
