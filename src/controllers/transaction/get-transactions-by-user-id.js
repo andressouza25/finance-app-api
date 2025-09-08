29959b12e4d26cb6ea7e6fa80721c96cdab0d666
André Santos <andressouza25@gmail.com>
2025-09-08 17:23:39 -0300

    feat: ensure from and to params are passed correctly to use case

--- src/controllers/transaction/get-transactions-by-user-id.js
+++ src/controllers/transaction/get-transactions-by-user-id.js
@@ -25,7 +25,11 @@ export class GetTransactionsByUserIdController {
             })
 
             const transactions =
-                await this.getTransactionByUserIdUseCase.execute(userId)
+                await this.getTransactionByUserIdUseCase.execute(
+                    userId,
+                    from,
+                    to,
+                )
 
             return ok(transactions)
         } catch (error) {
